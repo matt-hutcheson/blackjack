e4aba7b78abdb517c979864348d9ce3a85471a95
mjmccoach <mmccoach88@gmail.com>
2020-11-19 13:52:20 +0000

    Enum for suit and function for getting suit working

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,10 @@
 public class Card {
+    private SuitType suit;
+    
+    public Card(SuitType suit) {
+        this.suit = suit;
+    }
+    public SuitType getSuit() {
+        return this.suit;
+    }
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+    Card card;
+    @Before
+    public void before(){
+        card = new Card(SuitType.HEARTS);
+    }
+    @Test
+    public void cardHasSuit(){
+        assertEquals(SuitType.HEARTS, card.getSuit());
+    }
+
+    @Test
+    public void cardHasRank() {
+        assertEquals(RankType.TEN, card.getRank());
+    }
 }
